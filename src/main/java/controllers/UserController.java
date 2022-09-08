f5b6a7285dcd525db4e684ad7878cb850dc17994
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-08 15:56:03 -0300

    Criar classe da camada controller

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,9 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class UserController {
+}
