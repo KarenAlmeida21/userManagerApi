795a206f22c72f078fd80e37ec1f71af82325536
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-08 15:39:25 -0300

    modelar entidade user

---
+++ src/main/java/models/User.java
@@ -0,0 +1,26 @@
+package models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    private Long id;
+    @Column(nullable = false)
+    private String login;
+    @Column(nullable = false)
+    private String password;
+}
