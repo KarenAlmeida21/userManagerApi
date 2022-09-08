c4a38d54dffee2b5907efc8ce1ed39b8806db8e1
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-08 15:33:05 -0300

    first commit

---
+++ src/main/java/com/api/userManagerApi/UserManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.userManagerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagerApiApplication.class, args);
+	}
+
+}
