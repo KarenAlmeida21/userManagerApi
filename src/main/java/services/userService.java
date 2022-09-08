866fc083d48827e55c8933b91e01188b6534a1d4
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-08 15:50:23 -0300

    Adicionar metodo salvar user

--- src/main/java/services/userService.java
+++ src/main/java/services/userService.java
@@ -1,5 +1,6 @@
 package services;
 
+import models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.UserRepository;
@@ -8,4 +9,8 @@ import repositories.UserRepository;
 public class userService {
 @Autowired
     UserRepository userRepository;
+
+    public Object salvarUser(User user) {
+        return userRepository.save(user);
+    }
 }
