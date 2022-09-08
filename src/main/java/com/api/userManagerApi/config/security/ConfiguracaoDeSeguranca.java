3087b960a9f1238c58739b953c1836c2a9d9d231
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-08 20:02:49 -0300

    Implementando a classe de seguranca com as anatocoes

---
+++ src/main/java/com/api/userManagerApi/config/security/ConfiguracaoDeSeguranca.java
@@ -0,0 +1,12 @@
+package com.api.userManagerApi.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+}
