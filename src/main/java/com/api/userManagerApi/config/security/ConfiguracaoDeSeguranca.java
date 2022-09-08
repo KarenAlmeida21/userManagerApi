ff163437c5db2e024d5f5edcf376549afabc0524
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-08 20:07:21 -0300

    Desabilitando o crsf e configurando o Cors

--- src/main/java/com/api/userManagerApi/config/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/com/api/userManagerApi/config/security/ConfiguracaoDeSeguranca.java
@@ -1,6 +1,7 @@
 package com.api.userManagerApi.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,5 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http)throws Exception{
+        http.csrf();
 
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/user").permitAll()
+                .anyRequest().authenticated();
+
+    }
 }
