2e5dd1a116059201d093c0396d75dcad0450cf1c
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-08 20:32:59 -0300

    Adicionar classe loginDto

---
+++ src/main/java/com/api/userManagerApi/Dtos/LoginDto.java
@@ -0,0 +1,25 @@
+package com.api.userManagerApi.Dtos;
+
+public class LoginDto {
+    private String login;
+    private String password;
+
+    public LoginDto() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
