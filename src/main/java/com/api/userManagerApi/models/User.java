f11cddf346256d75b0eb0e1e06b49cca0c2c4fb9
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-12 16:18:06 -0300

    Adicionador constructor com argumentos por meio do lombok

--- src/main/java/com/api/userManagerApi/models/User.java
+++ src/main/java/com/api/userManagerApi/models/User.java
@@ -1,8 +1,10 @@
 package com.api.userManagerApi.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,6 +13,7 @@ import javax.persistence.Table;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
 
 
